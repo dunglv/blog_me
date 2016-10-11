8dfa50db2eadbf4ee1509aa361c642f6475b4eaa
Luong Viet Dung <Luong Viet Dung>
2016-10-11 17:08:13 +0700

    start head

--- index.php
+++ index.php
@@ -2,9 +2,95 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Luong Viet Dung</title>
+	<title>Text</title>
+	<link rel="stylesheet" href="css/site.css">
 </head>
 <body>
-	
+	<header>
+		<nav id="menu">
+			<div class="container">
+				<ul class="main-menu">
+					<li><a href="#">Trang chủ</a></li>
+					<li><a href="#">Giới thiệu</a></li>
+					<li><a href="#">Thông tin</a></li>
+					<li><a href="#">Học tập</a></li>
+					<li><a href="#">Sản phẩm</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Video</a></li>
+					<li><a href="#">Liên hệ</a></li>
+				</ul>
+			</div>
+		</nav>
+		<section id="quote">
+			<div class="container">
+				<div class="logo">
+					<img src="images/logo.png" alt="Lorem ipsum dolor.">
+				</div>
+				<h2>Lorem ipsum dolor.</h2>
+				<div class="quote-text">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda adipisci hic, quae!
+				</div>
+			</div>
+		</section>
+	</header>
+	<main>
+		<section id="personal">
+			<div class="container">
+				<div class="in-personal">
+					<div class="title">
+						<h3>
+							Lorem ipsum dolor sit.
+						</h3>
+					</div>
+					<div class="content">
+						<div class="col col-5">
+							<ul class="infor-block">
+								<li>
+									<div class="prop">name</div>
+									<div class="val">Lorem ipsum dolor.</div>
+								</li>
+								<li>
+									<div class="prop">name</div>
+									<div class="val">Lorem ipsum dolor.</div>
+								</li>
+								<li>
+									<div class="prop">name</div>
+									<div class="val">Lorem ipsum dolor.</div>
+								</li>
+								<li>
+									<div class="prop">name</div>
+									<div class="val">Lorem ipsum dolor.</div>
+								</li>
+							</ul>
+						</div> <!-- end col -->
+						<div class="col col-5">
+							<div class="infor-block">
+								<img src="images/logo.png" alt="Lorem ipsum dolor sit.">
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+		<section id="learning">
+			<div class="container"></div>
+		</section>
+		<section id="product">
+			<div class="container"></div>
+		</section>
+		<section id="blog">
+			<div class="container"></div>
+		</section>
+		<section id="video">
+			<div class="container"></div>
+		</section>
+		<section id="contact">
+			<div class="container"></div>
+		</section>
+	</main>
+	<footer>
+		
+	</footer>
 </body>
 </html>
